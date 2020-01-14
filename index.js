60e16cb47e3175e206665689176e142c9a9c287f
shams khalid <shamsnaji4@gmail.com>
2020-01-14 17:56:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 class Formatter {
   //add static methods here
   
- static capitalize(str){return str.toUppercase(); }
+ static capitalize(str){
+   return str.toUppercase(); }
  
  static sanitize(str){
     for(let i=0; i<str.length; i++)
